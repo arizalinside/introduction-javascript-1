0eb68566e3da3d0cbaab4e2cb1931f6fa91a01c9
Arizal Sykes <arizalinc@gmail.com>
2020-07-29 08:21:39 +0700

    revisi

--- week-2/dataArray.js
+++ week-2/dataArray.js
@@ -1,13 +1,13 @@
 function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
-    let filteredNumbers = dataArray.filter((number) => number > nilaiAwal && number < nilaiAkhir).sort(function(a,b){return a-b})
-    
+    let filteredNumbers = dataArray.filter((number) => number > nilaiAwal && number < nilaiAkhir).sort((a,b) => a-b)
+
     if (nilaiAkhir < nilaiAwal) {
         console.log("Nilai akhir harus lebih besar dari nilai awal")
-    } else if (dataArray < 5) {
+    } else if (dataArray.length < 5) {
+        console.log("dataArray harus lebih dari 5")
+    } else if (filteredNumbers == null) {
         console.log("Jumlah angka dalam dataArray tidak ada")
-    } 
+    }
     console.log(filteredNumbers)
 }
-
-
 seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])
